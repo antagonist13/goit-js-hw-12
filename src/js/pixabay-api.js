f84873b3f22fa42786783dd2a7a890aaad472a2a
antagonist13 <1703shakayaroslaw@gmail.com>
2024-04-05 18:18:52 +0300

    add loader

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
-export async function getImages(query) {
+export async function getImages(query, page) {
 const BASE_URL = 'https://pixabay.com';
 const END_POINT = '/api/';
 const searchParams = new URLSearchParams({
-        key:'43176233-1ec673b2575ffa542635b4c36',
-        q: `${query}`,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true
+    key:'43176233-1ec673b2575ffa542635b4c36',
+    q: `${query}`,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: `${page}`,
+    per_page: 15   
 });
     const url = `${BASE_URL}${END_POINT}?${searchParams}`;
     const res = await axios.get(url)
--- src/main.js
+++ src/main.js
@@ -11,39 +11,74 @@ const gallery = document.querySelector(".gallery")
 const searchForm = document.querySelector(".gallery-form")
 const searchInput = document.querySelector(".search-window")
 const loadingIndicator = document.querySelector('.loader');
+const loadBtn = document.querySelector('.load-btn')
+
+let query;
+let currentPage = 1;
+const pageSize = 15
+let maxPage = 1
 
 searchForm.addEventListener('submit', async (event) => {
     event.preventDefault()
-    loadingIndicator.style.display = 'block'
-
+    gallery.innerHTML = ""
+    query = searchInput.value
     try {
+        mainCreating()
+    } catch (error) {
+        console.log(error)
+    }
+    showLoader();
+    searchInput.value = ''
+    }
+)
+
+loadBtn.addEventListener('click', onLoaderClick)
+function onLoaderClick() {
+  currentPage += 1;
+    try {
+        mainCreating()
+    } catch (error) {
+        console.log(error)
+    } 
+//   myScroll();
+  checkBtnStatus();
+}
+
+async function imgCreating() {
+    const img = getImages(query, currentPage)
+    const res = await img
+    maxPage = Math.ceil(res.total / pageSize);
+    return img
+}
+function imgTemplate(arr) {
+    return arr.map(createMarkup).join('');         
+}
+function showLoader() {
+  loadBtn.classList.remove('hidden');
+}
+function hideLoader() {
+  loadBtn.classList.add('hidden');
+}
+function checkBtnStatus() {
+  if (currentPage >= maxPage) {
+    hideLoader();
+  } else {
+    showLoader();
+  }
+}
+async function mainCreating() {
+    loadingIndicator.style.display = 'block'
         const el = await imgCreating()
-     const imgList =  el.hits
-        if (imgList.length === 0) {
+        const imgList = el.hits
+        if (el.hits.length === 0) {
             iziToast.error({
             message: `Sorry, there are no images matching your search query. Please try again!`,
             position: 'topRight'
             });
         }
-    gallery.innerHTML = imgTemplate(imgList)
+    gallery.insertAdjacentHTML('beforeend', imgTemplate(imgList))
+    loadingIndicator.style.display = 'none'
+
     let lightbox = new SimpleLightbox('.gallery li a', { captionsData: 'alt', captionsDelay: 250 });
         lightbox.refresh()
-    loadingIndicator.style.display = 'none'
-    } catch (error) {
-        console.log(error)
-    }
-    searchInput.value = ''
-    
-        
-    }
-)
-
-
-function imgCreating() {
-    const img = getImages(searchInput.value)
-    return img
-}
-function imgTemplate(arr) {
-    return arr.map(createMarkup).join('');
-             
 }
